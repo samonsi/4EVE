67056f1894df264bb60f29b972a5c88b5a9975be
Samonsi <127188937+samonsi@users.noreply.github.com>
2025-09-18 00:04:27 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let allChannels = [];
 let defaultPlaylists = [
-    { name: "Local Playlist 1", type: "local", source: "playlist/playlist-1.m3u" },
+    { name: "Local Playlist 1", type: "local", source: "playlist-1.m3u" },
     { name: "Pastebin Playlist", type: "url", source: "https://pastebin.com/raw/2z6DGDgH" }
 ];
 let currentPlaylistName = "";
@@ -317,3 +317,4 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 });
+
